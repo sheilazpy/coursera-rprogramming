fd73d6cb01c78e23f028c4ce71341f4c58463e44
bertotte <m.a.otte@gmail.com>
2014-06-22 20:38:16 +0200

    ## added the code that: 1. Read the Dataset 2. Validates the passed function arguments 3. Returns the hospital name in the state with the lowest 30.day death rate.

--- ProgAssign3/best.r
+++ ProgAssign3/best.r
@@ -1,11 +1,32 @@
 best <- function(state, outcome) {
      
     ## Read outcome data
+	hospitalData <- read.csv("outcome-of-care-measures.csv",
+		colClasses = "character")
 
-	
-	## Check that state and outcome are valid
-	
-	
+	## State Validation
+	matchState <- match(state, hospitalData$State, nomatch = 0)
+	if (matchState == 0) stop("invalid state")
+
+	## Outcome Validation
+	validOutcomes <- c("heart attack", "heart failure", "pneumonia")
+	matchOutcome <- match(outcome, validOutcomes, nomatch = 0)
+	if (matchOutcome == 0) stop("invalid outcome")
+
 	## Return hospital name in that state with lowest 30-day death rate
+	## 1. First, select appropiate column depending on outcome
+	if (outcome == validOutcomes[1]) col <- as.integer(11) 	
+	if (outcome == validOutcomes[2]) col <- as.integer(17)	
+	if (outcome == validOutcomes[3]) col <- as.integer(23)
+
+	## 2. Create numeric vector of hospital data and carry out tapply
+	dataVector <- as.numeric(hospitalData[,col])
+	minValues <- tapply(dataVector, hospitalData$State, min, na.rm = TRUE)
+
+	## 3. Select entries hospitalData that represent minima for selected state
+	entry <- which(dataVector == minValues[state] & hospitalData$State == state)
+
+	## 4.
+	return(hospitalData[entry,2])
 
 }
