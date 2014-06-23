546ae0e8e08872c379ad7c79529d1c882ec31a25
bertotte <m.a.otte@gmail.com>
2014-06-23 13:25:19 +0200

    refactor

--- ProgAssign3/rankhospital.r
+++ ProgAssign3/rankhospital.r
@@ -9,29 +9,31 @@ rankhospital <- function(state, outcome, num = "best") {
 	if (matchState == 0) stop("invalid state")
 
 	## Outcome Validation
-	Outcomes <- c("heart attack", "heart failure", "pneumonia")
-	matchOutcome <- match(outcome, Outcomes, nomatch = 0)
+	out <- c("heart attack", "heart failure", "pneumonia")
+	matchOutcome <- match(outcome, out, nomatch = 0)
 	if (matchOutcome == 0) stop("invalid outcome")
 
 	## Return hospital name in that state with lowest 30-day death rate
 	## 1. First, subset the data depending on state and outcome
-	if (outcome == Outcomes[1]) subData <- subset(Data, State == state, select = c(2,11))
-	if (outcome == Outcomes[2]) subData <- subset(Data, State == state, select = c(2,17))	
-	if (outcome == Outcomes[3]) subData <- subset(Data, State == state, select = c(2,23))
+	if (outcome == out[1]) sData <- subset(Data, State == state, select = c(2,11))
+	if (outcome == out[2]) sData <- subset(Data, State == state, select = c(2,17))	
+	if (outcome == out[3]) sData <- subset(Data, State == state, select = c(2,23))
 
-  	##2. Second, order the subData table in descending order
-	subDataOrdered <- subData[order(as.numeric(subData[,2]), subData[,1], decreasing = c(F, F), na.last = TRUE),]
+  	##2. Second, order the sData table in descending order
+	Names <- sData[,1]
+	Rates <- as.numeric(sData[,2])
+	sDataOrd <- sData[order(Rates, Names, decreasing = c(F, F), na.last = TRUE),]
 	
-	##3. Subset "subDataOrdered"-entries with !na results
-	subDataOrdered <- subset(subDataOrdered, !is.na(as.numeric(subDataOrdered[,2])))
+	##3. Subset "sDataOrd"-entries with !na results
+	sDataOrd <- subset(sDataOrd, !is.na(as.numeric(sDataOrd[,2])))
 
 	##4. Result Conditioning
 	if (num == "best") num = as.integer(1)
-	if (num == "worst") num = as.integer(dim(subDataOrdered)[1])	
+	if (num == "worst") num = as.integer(dim(sDataOrd)[1])	
 
 	##5. Return Result
-	if (as.integer(num) > dim(subData)[1]) return(NA)
+	if (as.integer(num) > dim(sData)[1]) return(NA)
 
-	return(subDataOrdered[as.integer(num),1])
+	return(sDataOrd[as.integer(num),1])
 
 }
